929beef16a2430f0ad7f537302f43ec7e4b38357
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-18 16:24:00 +0800

    added system properties

--- workshop11/src/main/java/vttp/workshop/workshop11/Workshop11Application.java
+++ workshop11/src/main/java/vttp/workshop/workshop11/Workshop11Application.java
@@ -1,5 +1,9 @@
 package vttp.workshop.workshop11;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Workshop11Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Workshop11Application.class, args);
+		// SpringApplication.run(Workshop11Application.class, args);
+
+		SpringApplication app = new SpringApplication(Workshop11Application.class);
+
+		String port = "3000";	// default port
+		ApplicationArguments input = new DefaultApplicationArguments(args);
+
+		if (input.containsOption("port")) {
+			port = input.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
 	}
 
 }
