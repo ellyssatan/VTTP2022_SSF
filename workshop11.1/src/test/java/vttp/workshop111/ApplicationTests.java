66ac66235e1d9de3bf7201dc117c6ab66e43771a
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-18 10:28:33 +0800

    initial commit

---
+++ workshop11.1/src/test/java/vttp/workshop111/ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.workshop111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
